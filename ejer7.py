8dfec15f9f7fa4d75c952685257ec6e12b025eaa
benu510 <beatriz.nunez@babel.es>
2018-02-14 19:40:44 +0100

    parametros

---
+++ ejer7.py
@@ -0,0 +1,10 @@
+#funcion que devuelva con return par o impar
+
+def tiponumero(numero):
+	if numero%2==0:
+		return "par"
+	else:
+		return "impar"
+	
+
+
--- importafuncion.py
+++ importafuncion.py
@@ -3,3 +3,12 @@ import ejer6
 ejer6.imprimir()
 
 print ejer6.funcion()
+
+
+import ejer7
+
+print ejer7.tiponumero(6)
+
+import ejer8
+
+ejer8.alta_cliente("28817801c",'bea','nunez','quintana')
