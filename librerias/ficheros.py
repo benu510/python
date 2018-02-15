2eafa1173ff201a0c19e590e8c135956770abb51
root <root@localhost.localdomain>
2018-02-15 18:30:01 +0100

    ficheros

--- librerias/ficheros.py
+++ librerias/ficheros.py
@@ -1,3 +1,26 @@
+
+import os
+
 def version():
 	return 1.0
 
+
+
+
+def creadir(dir):
+	 
+
+	ruta ='/tmp/' + dir
+	if  os.access (ruta,1):
+		return "el archivo ya existe"
+
+	os.mkdir (ruta) 
+	return "Archivo creado"
+
+
+def entorno ():
+	
+	
+	for x,a in os.environ.iteritem():
+		if x = 'USER' lr clave=='PATH' or clave=='SHELL':
+			print valor
