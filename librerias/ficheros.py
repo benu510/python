cac59df28e2fc5420f13572e8c46f22b05199fc2
root <root@localhost.localdomain>
2018-02-26 16:02:53 +0100

    ficheros cp

--- librerias/ficheros.py
+++ librerias/ficheros.py
@@ -24,3 +24,25 @@ def entorno ():
 	for x,a in os.environ.iteritem():
 		if x = 'USER' lr clave=='PATH' or clave=='SHELL':
 			print valor
+
+
+
+
+
+#Similar a cp.
+def cp(forig,fdest):
+	if not os.access(forig,0):
+		print 'No existe el orígen.'
+		exit()
+	if os.access(fdest,0):
+		print 'Ya existe destino.'
+		exit()
+	if not os.path.isfile(forig):
+		print 'No es un archivo.'
+		exit()
+	origen=open(forig,'r')
+	destino=open(fdest,'w')
+	destino.writelines(origen)
+	origen.close()
+	destino.close()
+
